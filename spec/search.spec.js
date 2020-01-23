d4af5b43fbf13d1e534511d493ae444ec9240a7b
Girma Nigusse <xgirma@gmail.com>
2020-01-23 01:50:00 -0500

    update tests

--- spec/search.spec.js
+++ spec/search.spec.js
@@ -1,27 +1,13 @@
-import { ExpectedConditions, Key } from "protractor";
-
-describe("google search", () => {
+describe("Sandbox", () => {
   beforeAll(() => {
-    browser.waitForAngularEnabled(false);
-    browser.get("https://www.google.com/");
+    browser.get("https://e2e-boilerplates.github.io/sandbox/");
   });
 
-  it("should be on google search page", async () => {
+  it("should be on Sandbox", async () => {
     const title = await browser.getTitle();
-    expect(title).toEqual("Google");
-  });
+    const header = element(by.css("h1"));
 
-  it("should search for Cheese!", () => {
-    const searchBox = element(by.name("q"));
-    expect(searchBox.isDisplayed()).toBe(true);
-    searchBox.sendKeys("Cheese!", Key.ENTER);
-  });
-
-  it('the page title should start with "Cheese!"', async () => {
-    browser.wait(ExpectedConditions.urlContains("search"), 5000);
-
-    const title = await browser.getTitle();
-    const words = title.split(" ");
-    expect(words[0]).toBe("Cheese!");
+    expect(title).toEqual("Sandbox");
+    expect(header.getText()).toEqual("Sandbox");
   });
 });
